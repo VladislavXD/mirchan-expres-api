8a2cc0024bde9dae197990f7f860388af9b36ba0
Vladislav <neivo@vladislav.dev>
2025-08-02 00:35:33 +0500

    update

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 // Раздаем статический файлы из папки uploads
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 
